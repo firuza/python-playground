04cc1d6c04ff299eef0aa5d5dfe057833fbf5571
firuza <firya87@gmail.com>
2022-03-07 19:23:06 +0530

    return None if number is negative for computing factorial

--- fact.py
+++ fact.py
@@ -1,6 +1,8 @@
 def factorial(number):
-    answer = 1
-    for i in range(1, number+1):
-        answer = answer * i
-    print(answer)
-    return(answer)
+    if number > 0:
+        answer = 1
+        for i in range(1, number+1):
+            answer = answer * i
+        print(answer)
+        return(answer)
+    return(None)
